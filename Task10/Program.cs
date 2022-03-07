a156e9e3cba76e06bf0872280eca73d71ebe7687
Svetlana Popova <sspopova10-08@yandex.ru>
2022-03-07 19:15:07 +0300

    Task 10 completed

---
+++ Task10/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int GetSecondLetterOfNumber(int number) //вычислет вторую цифру числа
+{
+    int result = number / 10 % 10;
+    return result;
+}
+
+Console.Write("Введите трехзначное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber >= 100 && userNumber < 1000) //проверяем число на трехзначность
+{
+    int result = GetSecondLetterOfNumber(userNumber);
+    Console.WriteLine($"У числа {userNumber} вторая цифра равна {result}");
+}
+else
+{
+    Console.WriteLine("Введите трехзначное число!");
+}
+
